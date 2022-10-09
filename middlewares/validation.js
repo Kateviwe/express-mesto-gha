e4b9890f5551b9539e7146686f3ca37fa33b7bc5
Ekaterina <67003252+Kateviwe@users.noreply.github.com>
2022-10-09 16:34:43 +0300

    fix: errors

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -42,7 +42,7 @@ const postNewUserValidation = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30).default('Жак-Ив Кусто'),
     about: Joi.string().min(2).max(30).default('Исследователь'),
-    avatar: Joi.string().default('https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png'),
+    avatar: Joi.string().default('https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png').regex(RegExp(regExpUrl)),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
