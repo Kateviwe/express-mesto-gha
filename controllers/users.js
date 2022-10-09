ce0f91371720c55c1e48b05a70867a7b2f4dd770
Ekaterina <67003252+Kateviwe@users.noreply.github.com>
2022-10-09 15:45:50 +0300

    fix: return json object

--- controllers/users.js
+++ controllers/users.js
@@ -145,6 +145,7 @@ module.exports.login = (req, res, next) => {
         NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
         { expiresIn: '7d' },
       );
+      res.send({ email });
       // Запишем токен в куки, и опцией maxAge определим время хранения куки: 7 дней
       // 'httpOnly: true' - доступ из JavaScript запрещен
       // 'sameSite: true' - защита от автоматической отправки кук
